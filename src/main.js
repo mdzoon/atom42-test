57e6b50c61acfec601ecf7545bbc71507111be3a
Michael Dyczkowski <michaeldyczkowski@gmail.com>
2021-03-15 09:37:19 +0000

    registered Stripe

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
+import { StripePlugin } from '@vue-stripe/vue-stripe';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+const options = {
+  pk: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeAccount: process.env.STRIPE_ACCOUNT,
+  apiVersion: process.env.API_VERSION,
+  locale: process.env.LOCALE,
+};
+
 Vue.use(Vuelidate)
+Vue.use(StripePlugin, options)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
